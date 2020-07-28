9e8496735746a25cccd078462d083a68cc2efd33
ngocnhi10011999 <ngocnhinguyen1001@gmail.com>
2020-07-29 06:55:01 +0700

    First task

---
+++ app/src/main/java/com/example/mydictionary/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mydictionary.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
